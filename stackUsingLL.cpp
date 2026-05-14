733ae85c35861ae4e88c5664e6a4bdba204e6da2
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-05-14 11:03:33 +0700

    menambahkan default

--- stackUsingLL.cpp
+++ stackUsingLL.cpp
@@ -118,4 +118,12 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
-        
+        default:
+            cout << "Invalid choice. try again." << endl;
+            break;
+        }
+        cout << endl;
+
+    }
+        return 0;
+};               
