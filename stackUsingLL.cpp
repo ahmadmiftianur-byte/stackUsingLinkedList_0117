87210a63e7fea048ba1ffdb73deccb938be63ab9
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-05-14 10:52:37 +0700

    membuat fungsi pop

--- stackUsingLL.cpp
+++ stackUsingLL.cpp
@@ -35,4 +35,16 @@ public:
         return value;
     }
 
+    void pop()
+    {
+        if (isEmpty()) 
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
+
     
